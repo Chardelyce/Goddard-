25afdf20148972d8481e4c20346ec62e302a5b69
Chardelyce <63970461+Chardelyce@users.noreply.github.com>
2021-09-16 18:40:47 -0500

    added  prototype beta version :)
    this was the very first version of goddard i used to be really proud of this one now looking back its very elementary

---
+++ GoddardAI/GraphicsPanel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hellobox
+{
+    class GraphicsPanel : Panel
+    {
+        // Default constructor
+        public GraphicsPanel()
+        {
+            // Turn on double buffering.
+            this.DoubleBuffered = true;
+
+            // Allow repainting when the window is resized.
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+    }
+}
